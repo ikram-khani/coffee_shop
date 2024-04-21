d2a3e1d52987c671cca4c4a4bae23e0f0da47edc
ikram <ikr302000@gmail.com>
2024-04-21 22:05:44 +0500

    check if permission is not granted so turn off the switch

--- lib/view_models/location_data_provider.dart
+++ lib/view_models/location_data_provider.dart
@@ -4,32 +4,39 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LocationDataProvider with ChangeNotifier {
-  bool _isLocEnabled = false;
-
-  bool get isLocEnabled => _isLocEnabled;
+  bool isLocEnabled = false;
 
   Future<void> loadLocationEnabledState() async {
+    final permissionStatus = await Permission.locationWhenInUse.status;
+    bool isLocationServiceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!permissionStatus.isGranted || !isLocationServiceEnabled) {
+      saveLocationEnabledState(false);
+      notifyListeners();
+    }
     SharedPreferences prefs = await SharedPreferences.getInstance();
 
-    _isLocEnabled = prefs.getBool('isLocEnabled') ?? false;
+    isLocEnabled = prefs.getBool('isLocEnabled') ?? false;
     notifyListeners();
   }
 
   Future<void> saveLocationEnabledState(bool value) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    _isLocEnabled = value;
+    isLocEnabled = value;
     await prefs.setBool('isLocEnabled', value);
     notifyListeners();
   }
 
   Future<void> requestLocationPermission() async {
     final permissionStatus = await Permission.locationWhenInUse.status;
+
     if (permissionStatus.isGranted) {
       // Permission already granted, proceed with location access
       _getCurrentLocation();
       saveLocationEnabledState(true);
+      // Save state to shared preferences
     } else {
       final locationStatus = await Permission.locationWhenInUse.request();
+
       if (locationStatus.isGranted) {
         _getCurrentLocation();
         saveLocationEnabledState(true);
@@ -47,6 +54,8 @@ class LocationDataProvider with ChangeNotifier {
       print('Latitude: ${position.latitude}, Longitude: ${position.longitude}');
     } catch (error) {
       print('Error getting location: $error');
+      saveLocationEnabledState(false);
+      notifyListeners();
     }
   }
 }
